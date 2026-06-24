93bfee493e4bd16cb6012f0a0b526dca303184c6
Tatiana Kh <ts.khol@gmail.com>
2026-06-24 15:09:31 +0300

    26.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
-  description = "hibi-hono";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  description = "hibi-api";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
 
   outputs = {
     self,
@@ -11,18 +11,18 @@
       pkgs = import nixpkgs {inherit system;};
     in {
       packages.default = pkgs.buildNpmPackage {
-        name = "hibi-hono";
+        name = "hibi-api";
         src = ./.;
         npmDepsHash = "sha256-raFZZYXdTMRw6mU5/k7lFZHJo/N6Soml59Q3TiNtdA8=";
         nodejs = pkgs.nodejs_24;
         installPhase = ''
-          mkdir -p $out/lib/hibi-hono $out/bin
-          cp -r dist node_modules package.json $out/lib/hibi-hono/
-          cat > $out/bin/hibi-hono <<EOF
+          mkdir -p $out/lib/hibi-api $out/bin
+          cp -r dist node_modules package.json $out/lib/hibi-api/
+          cat > $out/bin/hibi-api <<EOF
           #!${pkgs.runtimeShell}
-          exec ${pkgs.nodejs_24}/bin/node $out/lib/hibi-hono/dist/index.js "\$@"
+          exec ${pkgs.nodejs_24}/bin/node $out/lib/hibi-api/dist/index.js "\$@"
           EOF
-          chmod +x $out/bin/hibi-hono
+          chmod +x $out/bin/hibi-api
         '';
       };
 
